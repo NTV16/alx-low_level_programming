1a1b7081ee275f7ae661f071dfdd3e055ab97871
NTV16 <taamziz16@gmail.com>
2023-06-16 03:25:29 -0700

    4. Hello, putsss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
-/*program that prints without printf*/
-
+/**
+ * main - Entry point
+ *
+ * Description: printing without using printf function
+ * @puts output parameter
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
