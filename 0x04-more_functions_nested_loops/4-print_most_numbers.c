a784ca68c563cef8e6017e470a51a29bde0017b4
NTV16 <taamziz16@gmail.com>
2023-06-22 05:36:13 -0700

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_most_numbers - starting point
+  *Return :void
+  */
+void print_most_numbers(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		if (b != 2 && b != 4)
+		{
+			_putchar (b + '0');
+		}
+		_putchar ('\n');
+	}
+}
