98e70aaebeaca0b0acd90f9b41d38456655dbcc7
NTV16 <taamziz16@gmail.com>
2023-06-27 11:07:47 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+  *_strcpy - starting point
+  *@dest: first operand
+  *@src: second operand
+  *Return: dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int r;
+
+	for (r = 0; r < _strlen(src); r++)
+	{
+		dest[r] = src[r];
+	}
+	return (dest);
+}
