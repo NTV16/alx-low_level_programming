2f0010a3358fb5848fba5840d68d8e4918293001
NTV16 <taamziz16@gmail.com>
2023-07-05 05:43:17 -0700

    2. Dreams feel real

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_strlen_recursion - starting point
+  *@s: string
+  *Return: integer
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		s++;
+	}
+	return ((*s + 1) / 8);
+}
