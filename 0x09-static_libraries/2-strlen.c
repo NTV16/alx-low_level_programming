d8ed981c8cf06ee4f17283bf5d6cfaac55e6a081
NTV16 <taamziz16@gmail.com>
2023-07-07 05:51:40 -0700

    1. Without libraries what have we

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -3,14 +3,14 @@
 /**
   *_strlen - starting point
   *@s: pointer
-  *Return: lenght of the string
+  *Return: length of the string
   */
+
 int _strlen(char *s)
 {
 	int b;
 
 	for (b = 0; s[b] != '\0'; b++)
 	{}
-
 	return (b);
 }
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
-gcc -c *.c
-ar rcs liball.a *.o
-ar -t liball.a
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra *.c
+ar -rcs liball.a *.o
+ranlib liball.a
