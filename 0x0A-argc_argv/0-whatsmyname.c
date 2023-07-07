b709ae0fe0d1daafaae50a29e963eba83b2d0b7d
NTV16 <taamziz16@gmail.com>
2023-07-07 09:12:58 -0700

    0. It aint what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+  *main - starting point
+  *@argc: argument count
+  *@argv: argument vector
+  *Return: Always 0 Success
+  */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
