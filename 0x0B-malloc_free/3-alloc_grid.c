d394fc32064970c30582151403037f9f9bd93c07
NTV16 <taamziz16@gmail.com>
2023-07-11 04:25:39 -0700

    3. If you even dream of beating me you better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	x = malloc(sizeof(int) * height);
+	x = malloc(sizeof(int *) * height);
 	if (x == NULL)
 	{
 		free(x);
