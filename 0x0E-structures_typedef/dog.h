ad4111eff52595c614683c1dc90884541084fb95
NTV16 <taamziz16@gmail.com>
2023-07-17 13:29:11 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+#include <string.h>
+
+/**
+  *struct dog -includes variables of the structure
+  *@name: first string
+  *@age: float
+  *@owner: second string
+  *Return: nothing
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
