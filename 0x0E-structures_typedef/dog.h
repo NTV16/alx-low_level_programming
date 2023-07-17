ef1e182c77f649f916cfa245aeaa155a218558f4
NTV16 <taamziz16@gmail.com>
2023-07-17 16:12:26 -0700

    3. Outside of a dog a book is a mans best friend

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,18 @@
   *@owner: second string
   *Return: nothing
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 int main(void);
-
+int _strlen(char *str);
+char _strcpy(char *dest, char *src);
 #endif
