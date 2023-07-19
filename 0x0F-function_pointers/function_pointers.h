a224c245f98c277773cc570e43fbf6087a564033
NTV16 <taamziz16@gmail.com>
2023-07-19 05:15:28 -0700

    0. Whats my name part 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_Hi
-#include <stddef.h>
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
-
+int _putchar(char c);
 #endif
